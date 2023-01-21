3cf75b1697787564560da232a01d3eb77443ef4b
Dagi <dagi.zewdu.dz@gmail.com>
2023-01-21 21:18:08 +0300

    Initiated vue app

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Todos />
+</template>
+
+<script>
+import Todos from './components/Todos.vue';
+export default {
+  name: 'App',
+  components: {
+    Todos,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div> </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css" scoped>
+body {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
